17d187b8775f5359ed9583f5d8cdb8844e1e12aa
jacek <jacekjanusiewicz02@gmail.com>
2025-09-30 23:43:46 +0100

    test basic functionality, add gitignore and cmakelists

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using evalfunction = bool(*)(std::vector<bool>);
+
+bool alwaysTrueEval(std::vector<bool>){ return true; }
+bool alwaysFalseEval(std::vector<bool>){ return false; }
+
+bool ANDEval(std::vector<bool> inputs){
+    for(bool i : inputs){ if(!i) return false; }
+    return true;
+}
+
+bool OREval(std::vector<bool> inputs){
+    for(bool i : inputs){ if(i) return true; }
+    return false;
+}
+
+bool NOTEval(std::vector<bool> inputs){
+    return !inputs[0];
+}
+
+bool XOREval(std::vector<bool> inputs){
+    bool current = false;
+    for(bool i : inputs){ current = (current != i); }
+    return current;
+}
+
+
+struct node{
+
+    node(evalfunction inevalfunc, std::vector<std::shared_ptr<node>> inconnections) : 
+    evalfunc(inevalfunc), connections(inconnections) {}
+
+    std::vector<std::shared_ptr<node>> connections;
+    evalfunction evalfunc;
+
+    bool evaluate(){
+        std::vector<bool> inputs;
+        for(auto i : connections){
+
+            inputs.push_back(i->evaluate());
+
+        }
+        return evalfunc(inputs);
+    }
+
+};
+
+int main(){
+
+    auto Node1 = std::make_shared<node>(alwaysTrueEval);
+    auto Node2 = std::make_shared<node>(alwaysFalseEval);
+    auto Node3 = std::make_shared<node>(alwaysTrueEval);
+    auto Node4 = std::make_shared<node>(alwaysFalseEval);
+
+}
