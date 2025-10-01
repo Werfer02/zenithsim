51923b77977b1f267a49d5c61e8fbfc419406aaf
jacek <jacekjanusiewicz02@gmail.com>
2025-10-01 01:18:45 +0100

    realise why stuff wasnt working, fix

--- zenithsim/main.cpp
+++ zenithsim/main.cpp
@@ -5,7 +5,7 @@
 #include "evalfunction.hpp"
 #include "node.hpp"
 
-
+static_assert(std::is_same_v<decltype(&ANDEval), evalfunction>, "ANDEval type mismatch!");
 
 int main(){
 
@@ -13,5 +13,11 @@ int main(){
     auto Node2 = std::make_shared<node>(alwaysFalseEval);
     auto Node3 = std::make_shared<node>(alwaysTrueEval);
     auto Node4 = std::make_shared<node>(alwaysFalseEval);
+
+    auto Node5 = std::make_shared<node>(&ANDEval, std::vector<std::shared_ptr<node>>{Node1, Node2});
+    auto Node6 = std::make_shared<node>(&ANDEval, std::vector<std::shared_ptr<node>>{Node1, Node3});
+
+    std::cout << Node5->evaluate() << "\n";
+    std::cout << Node6->evaluate() << "\n";
 
 }
